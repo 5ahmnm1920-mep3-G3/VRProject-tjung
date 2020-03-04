ff6ab1a7c8b856138e34293988bd931937464094
ThomasJunger <thomasjunger@gmx.at>
2020-03-04 14:17:56 +0100

    added c# Script

---
+++ Assets/MyGame/Script/Ball_Juggle_Counter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Juggle_Counter : MonoBehaviour
+{
+    
+     void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Ball")
+        {
+            Debug.Log("Ball colided");
+        }
+    }
+}
+  
