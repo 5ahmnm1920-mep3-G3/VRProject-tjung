e0865317dc172be28f967c77529eb34f55a39258
ThomasJunger <thomasjunger@gmx.at>
2020-03-25 19:00:15 +0100

    created goal model and added it into the scene, also added new materials and a score-collider

---
+++ Assets/MyGame/Script/shootScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootScript : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * 200);
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
