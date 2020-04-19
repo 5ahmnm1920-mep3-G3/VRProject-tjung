866d041418c53f16ff3219fdfbbe14ade0b9defa
ThomasJunger <thomasjunger@gmx.at>
2020-04-20 00:02:14 +0200

    addes Dokumenation Folder with Pdf

---
+++ Assets/MyGame/Script/Ball_Juggle_Counter1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Juggle_Counter1 : MonoBehaviour
+{
+    
+     void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "BallCollider")
+        {
+            Debug.Log("Ball colided");
+            Score1.scoreValue1 += 1;
+        }
+        
+    }
+
+}
+  
--- Assets/MyGame/Script/shootScript.cs
+++ Assets/MyGame/Script/shootScript.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class shootScript : MonoBehaviour
 {
+    public Vector3 ForceVariable = new Vector3 (-150, 50, 0);
     void OnMouseDown()
     {
         GetComponent<Rigidbody>().AddForce(transform.forward * 200);
         GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    public void Shoot()
+    {
+        
+        
+            GetComponent<Rigidbody>().AddForce(ForceVariable);
+            print("yes");
+        
     }
 }
  
